42f9f91d9ed119008ead273b8e9a65adfd2620d1
amikipax <souikiamine@gmail.com>
2020-06-19 03:16:53 +0200

    IMPL specify a maximum number of backups to keep per source, if the number is exceeded, the oldest file is automatically sent to trash

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import json
 import shutil
 import datetime
 
+import send2trash
+
+timestamp_format = "%Y-%m-%dT%H_%M_%S"
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -13,10 +16,21 @@ for content in content_sources:
     now = datetime.datetime.utcnow()
 
     content_path = content["path"]
-    content_name = content_path.split("\\")[-1] + now.strftime("_%Y_%m_%dT%H_%M_%S_UTC")
+    now_str = now.strftime(timestamp_format)
     content_label = content["label"]
+    nmax_backups = content["nmax_backups"]
 
     target_root = content.get("target_dir", default_target_dir)
-    target_dir_path = os.path.join(target_root, content_label, content_name)
+    target_main_dir = os.path.join(target_root, content_label)
+    target_dir_version = os.path.join(target_main_dir, now_str)
 
-    shutil.copytree(content["path"], target_dir_path)
+    shutil.copytree(content["path"], target_dir_version)
+
+    backups_versions = os.listdir(target_main_dir)
+    if len(backups_versions) > nmax_backups:
+        backups_dates_map = {
+            datetime.datetime.strptime(timestamp_str, timestamp_format): timestamp_str
+            for timestamp_str in backups_versions
+        }
+        file_to_remove = backups_dates_map[min(backups_dates_map.keys())]
+        send2trash.send2trash(os.path.join(target_main_dir, file_to_remove))
